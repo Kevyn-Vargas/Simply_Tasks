c1d1ff2578f06b55fd7530eae8fb502022ffe35d
Kevin Vargas <kevargasmurcia@gmail.com>
2024-06-09 18:08:23 -0500

    CSS Styles Smartphone

--- src/EmptyTodos/index.js
+++ src/EmptyTodos/index.js
@@ -3,9 +3,9 @@ import photogirl from "../img/girl.png";
 
 function EmptyTodos() {
   return (
-    <div>
-      <h2 className="title-empty">You have no tasks yet, add the first one!</h2>
-      <img className="img" src={photogirl} alt="" />
+    <div className="div-empty">
+      <h2 className="title-empty">No task found, try again or create a new task.</h2>
+      <img className="img-girl" src={photogirl} alt="" />
     </div>
   );
 }
--- src/NewTask/index.js
+++ src/NewTask/index.js
@@ -4,13 +4,25 @@ import { TodoContext } from "../TodoContext";
 
 function NewTask({ children }) {
   const { name, isNameLoading } = React.useContext(TodoContext);
+  const [isMobile, setIsMobile] = React.useState(window.innerWidth < 480);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 480);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   return (
     <div className="new-task-contenedor">
       <h1 className="title-new-task">
-        Hello 
+        Hello
         {!isNameLoading ? (
           <span className="name-text">{`${name}`}! </span>
+        ) : isMobile ? (
+          <span className="loading-text"> User ... </span>
         ) : (
           <span className="loading-box">
             <div className="WH animation color"></div>
@@ -18,7 +30,7 @@ function NewTask({ children }) {
             <div className="WH animation color"></div>
           </span>
         )}
-         Welcome to Simply_Tasks!
+        Welcome to Simply_Tasks!
       </h1>
       <h3>Organize your day and stay productive.</h3>
       <h3 className="subtitle-new-task">Task Name</h3>
